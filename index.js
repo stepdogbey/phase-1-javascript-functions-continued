4796ee40c131247e6d0cbdb8f94e7aaa0df94b63
stephen <stevedogbey@gmail.com>
2022-06-20 23:02:56 +0000

    commit tree 9928e2ebdd20a22af2df94396549b86d8abf7a31

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(sentence = "roller-skate"){   
+    return `This Saturday, I want to ${sentence}!`;
+}
+saturdayFun()
+saturdayFun("pay football")
+
+function mondayWork( myActivity = 'go to the office'){
+    return `This Monday, I will ${myActivity}.`
+}
+mondayWork();
+mondayWork("go to school")
+
+function wrapAdjective(visualFl = "*"){
+    return function(msg = "a hard worker"){
+        return `You are ${visualFl}${msg}${visualFl}!`
+    }  
+}
+wrapAdjective()
